b2f0095c0a19efdc3df663cf5e0fdc6e0caaa410
王安妮 <pore0814@gmail.com>
2018-05-23 10:39:16 +0800

    點選dateCollectionView 下放Scrollview 會滑到指定頁面
    滑動下方Scrollview，上方CollectionView會跟著滑動到指定DateCollectionViewCell

---
+++ TravelSharing/Model/GradientView.swift
@@ -0,0 +1,21 @@
+//
+//  GradientView.swift
+//  TravelSharing
+//
+//  Created by 王安妮 on 2018/5/23.
+//  Copyright © 2018年 Annie. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
