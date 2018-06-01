2ae423ed1e96dbc1f9601a23969f0e1cab341a14
王安妮 <pore0814@gmail.com>
2018-06-01 10:57:13 +0800

    調大頭照

--- TravelSharing/Controller/Register/RegisterTableViewController.swift
+++ TravelSharing/Controller/Register/RegisterTableViewController.swift
@@ -105,18 +105,7 @@ class RegisterTableViewController: UITableViewController,FusumaDelegate {
     
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 3
+   
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 3
-    }
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
-        return cell!
-    }
 
-}
